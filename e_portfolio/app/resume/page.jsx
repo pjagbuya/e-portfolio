dcf6e2ffc6c805cd3a66bedc464312636335fc73
pjagbuya <54502889+pjagbuya@users.noreply.github.com>
2024-11-17 22:26:18 +0800

    new stuff

--- e_portfolio/app/resume/page.jsx
+++ e_portfolio/app/resume/page.jsx
@@ -1,7 +1,7 @@
 export default function Resume() {
     return (
       <main>
-        <h1 className=" mb-5 text-center text-6xl text-purple-200">
+        <h1 className="mb-5 text-center text-6xl text-purple-200">
           Resume   
         </h1>
         <div className="flex justify-center">
